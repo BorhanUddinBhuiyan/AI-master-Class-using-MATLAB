131f3e0db50e65bd940b9ebc68c87c5dadbfe381
Borhan Uddin Bhuiyan <borhanuddinbhuiyan303@gmail.com>
2021-01-24 00:34:48 +0600

    day-3 computer vision starting

---
+++ Day -3 (Computer Vision)/facialPartsDetectionUsingViolaJonsAlgo.m
@@ -0,0 +1,11 @@
+clc;
+clear;
+close all;
+detector=vision.CascadeObjectDetector; %Detect Object using th Voila-Jones algorithm
+%reading image
+a=imread('3.jpg');
+imshow(a); %showing the image
+BoundingBox=step(detector,a); %Step response of dynamic systems.
+RGB =insertObjectAnnotation(a,'rectangle',BoundingBox, 'Face');
+figure;
+imshow(RGB)
